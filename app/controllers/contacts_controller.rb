e36d418cd0f78ce47ce38aa69cf533d1484fdac2
Kevin Solorio <kevin@detone8.com>
2015-01-20 21:37:02 -0600

    add create route

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -13,3 +13,16 @@ get '/contacts/new' do
   @contact = Contact.new(params[:contact])
   erb :'contacts/new'
 end
+
+
+# POST ========================================================================
+
+post '/contacts' do
+  @contact = Contact.new(params[:contact])
+  if @contact.save
+    redirect '/contacts'
+  else
+    response.status = 422
+    erb :'contacts/new'
+  end
+end
