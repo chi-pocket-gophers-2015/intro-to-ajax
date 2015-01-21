8698f9b89886e136cdc2b6924f3dcdfcfc638d0a
Kevin Solorio <kevin@detone8.com>
2015-01-20 21:12:40 -0600

    update controller to fetch data

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,5 +1,10 @@
 # GET =========================================================================
 
 get '/' do
+  redirect '/contacts'
+end
+
+get '/contacts' do
+  @contacts = Contact.all
   erb :'contacts/index'
 end
